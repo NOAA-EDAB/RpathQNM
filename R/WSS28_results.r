b6bd588c030580b073a6899c27987b29a879b931
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-08-05 15:09:01 -0400

    Perturbation results for WSS28

---
+++ R/WSS28_results.r
@@ -0,0 +1,17 @@
+#' Perturbation results for WSS28
+#'
+#' The number of positive increases resulting from 1000 ecosense runs of the 
+#' Western Scotian Shelf highly aggregated model (WSS28).  Perturbations included
+#' a 10% increase and decrease in the biomass of phytoplankton, small pelagics,
+#' and seals.  There is also a 10% increase and decrease in fishing effort. 
+#'
+#' @format A data frame
+#'
+#' \describe{
+#'\item{Group}{Perturbed group from WSS28}
+#'\item{Direction}{Plus is an increase of 10% while Minus is a decrease}
+#'
+#' }
+#'
+#'
+"WSS28.results"
